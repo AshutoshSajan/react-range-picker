6006b000db0d50dae08b533edadd26eb31a038ba
sajan <sajanloveu123@gmail.com>
2019-07-01 18:45:44 +0530

    edit readme.md

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+// import DatePicker from "my-react-date-picker";
+
 import './App.css';
 import DateRange from './DateRange';
+// import "my-react-date-picker/dist/react-datepicker.css";
 
 class App extends Component {
   
   render(){
     return (
       <div className="app" >
+      	{ /* <DatePicker /> */ }
         <DateRange />
       </div>
     )
